dca7675ef9328e8421519b1b12de946cf78bd845
mmakanda <mmakanda@users.noreply.github.com>
2026-05-10 15:54:51 +0200

    security: remove unsafe-eval from CSP, add X-XSS-Protection

--- next.config.js
+++ next.config.js
@@ -42,6 +42,10 @@ const nextConfig = {
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
+	  {
+  	    key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },	
           // Stop MIME-type sniffing
           {
             key: "X-Content-Type-Options",
@@ -68,7 +72,7 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com",
+              "script-src 'self' 'unsafe-inline' https://va.vercel-scripts.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https:",
